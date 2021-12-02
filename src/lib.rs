d512d0d5ddd1e4f8e3043cd50a8f518a12827dfe
Joel Courtney <joel.e.courtney@gmail.com>
2021-12-02 08:46:49 -0700

    added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,61 @@
+//! Boilerplate code for [mdbook](https://rust-lang.github.io/mdBook/index.html) preprocessors.
+//!
+//! Handles the CLI, checks whether the renderer is supported, checks the mdbook version, and runs
+//! your preprocessor. All you need to do is implement the [mdbook::preprocess::Preprocessor] trait.
+//!
+//! This boilerplate has a few heavy dependencies (like serde_json). If you want a small executable,
+//! you'll have to implement this functionality yourself.
+//!
+//! # Example
+//!
+//! The following is functionally identical to the [No-Op Preprocessor Example](https://github.com/rust-lang/mdBook/blob/master/examples/nop-preprocessor.rs)
+//! given by mdbook.
+//!
+//! ```no_run
+//! use mdbook::book::Book;
+//! use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
+//! use anyhow::{bail, Result};
+//!
+//! fn main() {
+//!     mdbook_preprocessor_boilerplate::run(
+//!         NoOpPreprocessor,
+//!         "An mdbook preprocessor that does nothing" // CLI description
+//!     );
+//! }
+//!
+//! struct NoOpPreprocessor;
+//!
+//! impl Preprocessor for NoOpPreprocessor {
+//!     fn name(&self) -> &str {
+//!         "nop-preprocessor"
+//!     }
+//!
+//!     fn run(&self, ctx: &PreprocessorContext, book: Book) -> Result<Book> {
+//!         // In testing we want to tell the preprocessor to blow up by setting a
+//!         // particular config value
+//!         if let Some(nop_cfg) = ctx.config.get_preprocessor(self.name()) {
+//!             if nop_cfg.contains_key("blow-up") {
+//!                 anyhow::bail!("Boom!!1!");
+//!             }
+//!         }
+//!
+//!         // we *are* a no-op preprocessor after all
+//!         Ok(book)
+//!     }
+//!
+//!     fn supports_renderer(&self, renderer: &str) -> bool {
+//!         renderer != "not-supported"
+//!     }
+//! }
+//! ```
+
 use mdbook::preprocess::{CmdPreprocessor, Preprocessor};
 use std::{process, io};
 use clap::{App, Arg, ArgMatches, SubCommand};
 use anyhow::Result;
 use semver::{Version, VersionReq};
 
+/// Checks renderer support and runs the preprocessor.
 pub fn run(preprocessor: impl Preprocessor, description: &str) {
     let matches = App::new(preprocessor.name())
         .about(description)
@@ -53,14 +105,5 @@ fn handle_supports(pre: impl Preprocessor, sub_args: &ArgMatches) -> ! {
         process::exit(0);
     } else {
         process::exit(1);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
     }
 }
